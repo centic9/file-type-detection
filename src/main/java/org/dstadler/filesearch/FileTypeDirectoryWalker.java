d655e38cdb83bac3d51d0f66524e209ee382d888
Dominik Stadler <dominik.stadler@gmx.at>
2016-06-23 12:26:59 +0200

    Make detection run in threads  Print some stats to stderr

--- src/main/java/org/dstadler/filesearch/FileTypeDirectoryWalker.java
+++ src/main/java/org/dstadler/filesearch/FileTypeDirectoryWalker.java
@@ -11,9 +11,15 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class FileTypeDirectoryWalker extends DirectoryWalker<Void> {
     private final DefaultDetector detector = new DefaultDetector();
+    private long count = 0;
+
+    private final ExecutorService executor = Executors.newFixedThreadPool(10);
 
     public FileTypeDirectoryWalker() {
         super();
@@ -21,17 +27,39 @@ public class FileTypeDirectoryWalker extends DirectoryWalker<Void> {
 
     @Override
     protected void handleFile(File file, int depth, Collection<Void> results) throws IOException {
-        try (TikaInputStream str = TikaInputStream.get(file.toPath())) {
-            final MediaType mediaType = detector.detect(str, new Metadata());
-            System.out.println("{ \"fileName\":\"" + file.getAbsolutePath() + "\", \"mediaType\":\"" + mediaType.toString() + "\"}");
-        }
+        count++;
+        executor.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    try (TikaInputStream str = TikaInputStream.get(file.toPath())) {
+                        final MediaType mediaType = detector.detect(str, new Metadata());
+
+                        // ensure that we do not mix output from different threads
+                        synchronized (this) {
+                            System.out.println("{ \"fileName\":\"" + file.getAbsolutePath() + "\", \"mediaType\":\"" + mediaType.toString() + "\"}");
+                        }
+                    }
+                } catch (IOException e) {
+                    System.err.println("Had exeception while handling file " + file + ": " + e);
+                    e.printStackTrace(System.err);
+                }
+            }
+        });
     }
 
-    public void execute(File startDir) throws IOException {
+    public long execute(File startDir) throws IOException, InterruptedException {
         Preconditions.checkNotNull(startDir, "Directory needs to be specified");
         Preconditions.checkState(startDir.exists(), "Directory %s needs to exist", startDir);
         Preconditions.checkState(startDir.isDirectory(), "Need to specify a directory, not a file, had %s", startDir);
 
         walk(startDir, Collections.emptyList());
+
+        executor.shutdown();
+        if(!executor.awaitTermination(1, TimeUnit.MINUTES)) {
+            throw new IllegalStateException("Could not wait for all threads to finish processing");
+        }
+
+        return count;
     }
 }
--- src/main/java/org/dstadler/filesearch/LookForFileType.java
+++ src/main/java/org/dstadler/filesearch/LookForFileType.java
@@ -1,10 +1,9 @@
 package org.dstadler.filesearch;
 
 import java.io.File;
-import java.io.IOException;
 
 public class LookForFileType {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         if(args.length == 0) {
             System.err.println("Usage: " + LookForFileType.class.getName() + " <directory> [<directory> ...]");
             return;
@@ -12,8 +11,13 @@ public class LookForFileType {
 
         // walk all arguments
         for(String location : args) {
+            long start = System.currentTimeMillis();
+            System.err.println("Processing directory " + location);
+
             FileTypeDirectoryWalker walker = new FileTypeDirectoryWalker();
-            walker.execute(new File(location));
+            long count = walker.execute(new File(location));
+
+            System.err.println("Found " + count + " files in directory '" + location + "', took " + (System.currentTimeMillis() - start) + "ms");
         }
     }
 }
