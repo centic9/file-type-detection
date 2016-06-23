39e733aaac785c5661189102dc88fc63667e274c
Dominik Stadler <dominik.stadler@gmx.at>
2016-06-23 12:06:01 +0200

    Initial implementation using Tika DefaultDetector()

--- src/main/java/org/dstadler/filesearch/FileTypeDirectoryWalker.java
+++ src/main/java/org/dstadler/filesearch/FileTypeDirectoryWalker.java
@@ -2,6 +2,10 @@ package org.dstadler.filesearch;
 
 import com.google.common.base.Preconditions;
 import org.apache.commons.io.DirectoryWalker;
+import org.apache.tika.detect.DefaultDetector;
+import org.apache.tika.io.TikaInputStream;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,13 +13,18 @@ import java.util.Collection;
 import java.util.Collections;
 
 public class FileTypeDirectoryWalker extends DirectoryWalker<Void> {
+    private final DefaultDetector detector = new DefaultDetector();
+
     public FileTypeDirectoryWalker() {
         super();
     }
 
     @Override
     protected void handleFile(File file, int depth, Collection<Void> results) throws IOException {
-
+        try (TikaInputStream str = TikaInputStream.get(file.toPath())) {
+            final MediaType mediaType = detector.detect(str, new Metadata());
+            System.out.println("{ \"fileName\":\"" + file.getAbsolutePath() + "\", \"mediaType\":\"" + mediaType.toString() + "\"}");
+        }
     }
 
     public void execute(File startDir) throws IOException {
--- src/main/java/org/dstadler/filesearch/LookForFileType.java
+++ src/main/java/org/dstadler/filesearch/LookForFileType.java
@@ -5,6 +5,11 @@ import java.io.IOException;
 
 public class LookForFileType {
     public static void main(String[] args) throws IOException {
+        if(args.length == 0) {
+            System.err.println("Usage: " + LookForFileType.class.getName() + " <directory> [<directory> ...]");
+            return;
+        }
+
         // walk all arguments
         for(String location : args) {
             FileTypeDirectoryWalker walker = new FileTypeDirectoryWalker();
