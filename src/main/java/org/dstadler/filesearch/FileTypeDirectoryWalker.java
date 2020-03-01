e85bca5231931ade4502c7f20d90261036312953
Dominik Stadler <dominik.stadler@gmx.at>
2020-03-01 09:26:31 +0100

    Adjust test to verify errors

--- src/main/java/org/dstadler/filesearch/FileTypeDirectoryWalker.java
+++ src/main/java/org/dstadler/filesearch/FileTypeDirectoryWalker.java
@@ -155,4 +155,8 @@ public class FileTypeDirectoryWalker extends DirectoryWalker<Void> {
 
         return stats;
     }
+
+    public long getErrorCount() {
+        return errorCount.get();
+    }
 }
--- src/test/java/org/dstadler/filesearch/FileTypeDirectoryWalkerTest.java
+++ src/test/java/org/dstadler/filesearch/FileTypeDirectoryWalkerTest.java
@@ -6,15 +6,20 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class FileTypeDirectoryWalkerTest {
     @Test
     public void testLocalFiles() throws IOException, InterruptedException {
-        MappedCounter<String> stats = new FileTypeDirectoryWalker().execute(new File("."));
+        FileTypeDirectoryWalker walker = new FileTypeDirectoryWalker();
+        MappedCounter<String> stats = walker.execute(new File("."));
 
         assertTrue(stats.sortedMap().keySet().size() > 10);
         assertTrue(stats.sortedMap().values().stream().mapToInt(value -> value).sum() > 100);
         assertTrue(stats.get("text/x-java-source") >= 3);
+
+        assertEquals("The invalid symbolic link should lead to an error",
+                1, walker.getErrorCount());
     }
 }
