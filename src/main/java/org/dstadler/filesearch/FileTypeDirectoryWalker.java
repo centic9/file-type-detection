228392a876b53fa7c7bcc66c711d5992b4d18019
Dominik Stadler <dominik.stadler@gmx.at>
2020-03-01 09:53:46 +0100

    Refactor to separate inner class for the Runnable
    Also improve output, do it periodically,

--- src/main/java/org/dstadler/filesearch/FileTypeDirectoryWalker.java
+++ src/main/java/org/dstadler/filesearch/FileTypeDirectoryWalker.java
@@ -3,7 +3,12 @@ package org.dstadler.filesearch;
 import com.google.common.base.Preconditions;
 import com.google.gson.JsonObject;
 import org.apache.commons.io.DirectoryWalker;
-import org.apache.commons.io.filefilter.*;
+import org.apache.commons.io.filefilter.AndFileFilter;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.NameFileFilter;
+import org.apache.commons.io.filefilter.NotFileFilter;
+import org.apache.commons.io.filefilter.OrFileFilter;
+import org.apache.commons.lang3.time.DateFormatUtils;
 import org.apache.tika.Tika;
 import org.apache.tika.io.TikaInputStream;
 import org.dstadler.commons.collections.ConcurrentMappedCounter;
@@ -34,6 +39,7 @@ public class FileTypeDirectoryWalker extends DirectoryWalker<Void> {
     private final AtomicLong count = new AtomicLong();
     private final AtomicLong submitCount = new AtomicLong();
     private final AtomicLong errorCount = new AtomicLong();
+    private final AtomicLong lastLog = new AtomicLong();
 
     private final ExecutorService executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
@@ -52,60 +58,12 @@ public class FileTypeDirectoryWalker extends DirectoryWalker<Void> {
     protected void handleFile(File file, int depth, Collection<Void> results) throws IOException {
         // count the file and possibly delay to not submit all files immediately which
         // would blow up memory-usage of the executor queue
-        countSubmitted();
-
-        // submit a job to detect the mime-type of this file
-        executor.submit(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    try (TikaInputStream str = TikaInputStream.get(file.toPath())) {
-                        final String mediaType = tika.detect(str, file.getName());
-
-                        // we found a mime-type, print out the JSON for it
-                        recordMediaType(mediaType);
-                    }
-                } catch (FileSystemException e) {
-                    // report some types of exception without stacktrace
-                    System.err.println("Had exception while handling file " + file + ": " + e);
-                    errorCount.incrementAndGet();
-                } catch (IOException e) {
-                    System.err.println("Had exception while handling file " + file + ": " + e);
-                    e.printStackTrace(System.err);
-                    errorCount.incrementAndGet();
-                } finally {
-                    long curr = count.incrementAndGet();
-                    if (curr % 1000 == 0) {
-                        System.err.print(".");
-                        if (curr % 100000 == 0) {
-                            System.err.println(submitCount.get() + "/" + curr);
-                        }
-                    }
-                }
-            }
-
-            private void recordMediaType(String mediaType) {
-                JsonObject json = new JsonObject();
-                json.addProperty("fileName", file.getAbsolutePath());
-                json.addProperty("mediaType", mediaType);
-
-                // synchronize to ensure that we do not mix output from different threads
-                synchronized (this) {
-                    //System.out.println("{ \"fileName\":\"" + file.getAbsolutePath() + "\", \"mediaType\":\"" + mediaType + "\"}");
-                    System.out.println(json);
-                }
-
-                stats.addInt(mediaType, 1);
-            }
-        });
-    }
-
-    private void countSubmitted() throws IOException {
         long submitted = submitCount.incrementAndGet();
         if(submitted - count.get() > 2000) {
             // if there are more than 2000, wait until we are below 1000 again to not fill up memory with all the submitted tasks
             while (submitted - count.get() > 1000) {
-                System.err.println("Delaying submitting a bit to not build up too many submitted jobs, having " + submitted + " overall, " + count.get() +
+                System.err.println("Delaying submitting a bit to not build up too many submitted jobs," +
+                        " having " + submitted + " files overall, " + count.get() +
                         " done, " + errorCount.get() + " errors, " + (submitted - count.get()) +
                         " currently queued, waiting for this number to be below 1000 before adding new jobs.");
                 try {
@@ -115,6 +73,9 @@ public class FileTypeDirectoryWalker extends DirectoryWalker<Void> {
                 }
             }
         }
+
+        // submit a job to detect the mime-type of this file
+        executor.submit(new FileTypeDetector(file));
     }
 
     /**
@@ -158,5 +119,57 @@ public class FileTypeDirectoryWalker extends DirectoryWalker<Void> {
 
     public long getErrorCount() {
         return errorCount.get();
+    }
+
+    private class FileTypeDetector implements Runnable {
+        private final File file;
+
+        public FileTypeDetector(File file) {
+            this.file = file;
+        }
+
+        @Override
+        public void run() {
+            try {
+                try (TikaInputStream str = TikaInputStream.get(file.toPath())) {
+                    final String mediaType = tika.detect(str, file.getName());
+
+                    // we found a mime-type, print out the JSON for it
+                    recordMediaType(mediaType);
+                }
+            } catch (FileSystemException e) {
+                // report some types of exception without stacktrace
+                System.err.println("Had exception while handling file " + file + ": " + e);
+                errorCount.incrementAndGet();
+            } catch (IOException e) {
+                System.err.println("Had exception while handling file " + file + ": " + e);
+                e.printStackTrace(System.err);
+                errorCount.incrementAndGet();
+            } finally {
+                long curr = count.incrementAndGet();
+                long currentTime = System.currentTimeMillis();
+
+                if(lastLog.get() < (currentTime - TimeUnit.SECONDS.toMillis(10))) {
+                    lastLog.set(currentTime);
+                    System.err.println(DateFormatUtils.ISO_8601_EXTENDED_DATETIME_FORMAT.format(currentTime) +
+                            " submitted: " + submitCount.get() + ", queued: " + count.get() + ", done: " + curr + ", error: " + errorCount.get() +
+                            ", current file: " + file);
+                }
+            }
+        }
+
+        private void recordMediaType(String mediaType) {
+            JsonObject json = new JsonObject();
+            json.addProperty("fileName", file.getAbsolutePath());
+            json.addProperty("mediaType", mediaType);
+
+            // synchronize to ensure that we do not mix output from different threads
+            synchronized (this) {
+                //System.out.println("{ \"fileName\":\"" + file.getAbsolutePath() + "\", \"mediaType\":\"" + mediaType + "\"}");
+                System.out.println(json);
+            }
+
+            stats.addInt(mediaType, 1);
+        }
     }
 }
