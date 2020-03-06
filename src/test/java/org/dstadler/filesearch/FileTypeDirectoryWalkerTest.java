0e171e804692fd9eb0890a0a45d394b0038309ac
Dominik Stadler <dominik.stadler@gmx.at>
2020-03-06 22:27:16 +0100

    Add more output to test-assertion

--- src/test/java/org/dstadler/filesearch/FileTypeDirectoryWalkerTest.java
+++ src/test/java/org/dstadler/filesearch/FileTypeDirectoryWalkerTest.java
@@ -13,11 +13,15 @@ public class FileTypeDirectoryWalkerTest {
     @Test
     public void testLocalFiles() throws IOException, InterruptedException {
         FileTypeDirectoryWalker walker = new FileTypeDirectoryWalker();
-        MappedCounter<String> stats = walker.execute(new File("."));
+        File startDir = new File(".");
+        MappedCounter<String> stats = walker.execute(startDir);
 
-        assertTrue(stats.sortedMap().keySet().size() > 10);
-        assertTrue(stats.sortedMap().values().stream().mapToInt(value -> value).sum() > 100);
-        assertTrue(stats.get("text/x-java-source") >= 3);
+        assertTrue("Had: " + startDir.getAbsolutePath() + " and " + stats.sortedMap().keySet() + " and " + stats.sortedMap(),
+                stats.sortedMap().keySet().size() > 10);
+        assertTrue("Had: " + startDir.getAbsolutePath() + " and " + stats.sortedMap().keySet() + " and " + stats.sortedMap(),
+                stats.sortedMap().values().stream().mapToInt(value -> value).sum() > 100);
+        assertTrue("Had: " + startDir.getAbsolutePath() + " and " + stats.sortedMap().keySet() + " and " + stats.sortedMap(),
+                stats.get("text/x-java-source") >= 3);
 
         assertEquals("The invalid symbolic link should lead to an error",
                 1, walker.getErrorCount());
